cb8f8b353b17f80ff036db827771ea305f266b7e
Paul Rybitskyi <Paul.Rybitskyi@gmail.com>
2021-04-09 20:22:49 +0300

    Update dependencies (#21)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -29,6 +29,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.1.1")
+    implementation("com.android.tools.build:gradle:4.1.3")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
 }
