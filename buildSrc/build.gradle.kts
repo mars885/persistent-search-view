cd4b04202c4ccbfb387e5ccd65fff073fbb34a80
Paul Rybitskyi <paul.rybitskyi@gmail.com>
2022-08-06 18:08:52 +0300

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -24,6 +24,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.0.3")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    implementation("com.android.tools.build:gradle:7.2.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
 }
